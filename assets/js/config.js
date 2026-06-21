168e808ff5505e134072f3f1fecc36d378a94760
lantran12 <lantran.labety@gmail.com>
2026-06-21 21:31:14 +0700

    Create config.js

---
+++ assets/js/config.js
@@ -0,0 +1,7 @@
+const GENDERS = [
+    { id: "vo-han", name: "Vô Hạn Lưu" },
+    { id: "tien-hiep", name: "Tiên hiệp" },
+    { id: "guong-vo", name: "Gương vỡ lại lành" },
+    { id: "trinh-tham", name: "Trinh thám" },
+    { id: "hai-huoc", name: "Hài hước" }
+];
