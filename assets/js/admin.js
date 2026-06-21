fd252171a3fcc29dd38ecbe32cbc3faa44c62831
lantran12 <lantran.labety@gmail.com>
2026-06-22 01:15:14 +0700

    Update admin.js

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,1 +1,12 @@
-
+// File: assets/js/admin.js
+function tangLuotXem(truyenId) {
+    if (!truyenId) return;
+    
+    // Trỏ tới đúng vị trí truyenId trong nhánh 'views' trên Firebase
+    const viewRef = firebase.database().ref('views/' + truyenId);
+    
+    // Tăng giá trị lên 1
+    viewRef.transaction(function(currentViews) {
+        return (currentViews || 0) + 1;
+    });
+}
