0420b2a2b306700743f992600715fb82615f905a
lantran12 <lantran.labety@gmail.com>
2026-06-21 16:49:44 +0700

    Create studio.js

---
+++ assets/js/studio.js
@@ -0,0 +1,28 @@
+// ==========================================================================
+// 1. CẤU HÌNH FIREBASE 
+// ==========================================================================
+const firebaseConfig = {
+    apiKey: "AIzaSyBimiEGQcW9at2pOxfdUaJHjim2fmyjjcc",
+    authDomain: "dongchanrua.firebaseapp.com",
+    databaseURL: "https://dongchanrua-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "dongchanrua",
+    storageBucket: "dongchanrua.firebasestorage.app",
+    messagingSenderId: "640115424540",
+    appId: "1:640115424540:web:c9713b7921c09283150ed9"
+};
+
+// Khởi tạo Firebase
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+const db = firebase.database();
+const auth = firebase.auth();
+
+// Chạy các hàm chức năng khi trang tải xong
+document.addEventListener('DOMContentLoaded', () => {
+    initSubBarFilters();
+    loadGenresDropdown();
+    loadMainStories();
+    loadTopViews();
+    listenToNotifications();
+});
